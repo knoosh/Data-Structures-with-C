a33a0432655fec19981f4f3f16afc95d46e25751
knoosh <88944116+knoosh@users.noreply.github.com>
2023-04-06 11:06:09 +0530

    Add files via upload

---
+++ ARRAY.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int main()
+
+{
+	int arr[10],i,a;                       //array for storing integers of size 10
+    printf("Enter the value of array\n");
+
+	for( a= 0; a<=5 ; a++ )
+	{
+	scanf("%d" , &x[a]);          //seeking input
+    }
+     printf("%d\n", x[a]);
+    scanf("%d", &i);
+
+    if(i>=0 && i<=9)
+	{
+		printf("the array location exists\n");
+    printf("%d",x[i]);
+	}
+	else 
+	{
+    printf("memory location does not exist");
+	}
+	return 0;
